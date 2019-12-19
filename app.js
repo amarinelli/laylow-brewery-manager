3d176c9a10e091dd353ec03c1c885504a75682ca
Adam Marinelli <a.marinelli16@gmail.com>
2019-12-19 02:04:13 -0500

    App home + button + basic modal

--- app.js
+++ app.js
@@ -8,6 +8,43 @@ require('dotenv').config()
 
 const appId = process.env.APP_ID;
 const botToken = process.env.BOT_TOKEN;
+const airtableToken = process.env.AIRTABLE_TOKEN;
+
+let appHomeView = {
+    "type": "home",
+    "callback_id": "brewery-manager-app-home",
+    "blocks": [
+        {
+            "type": "actions",
+            "block_id": "get-started-home-action",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Get Started",
+                        "emoji": true
+                    },
+                    "value": "get-started-home-button"
+                }
+            ]
+        },
+        {
+            "type": "image",
+            "image_url": "https://laylow.beer/img/laylow-brewery.jpg",
+            "alt_text": "Laylow Brewery logo"
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "mrkdwn",
+                    "text": "Last updated Dec 19, 2019"
+                }
+            ]
+        }
+    ]
+}
 
 var app = express();
 
@@ -64,15 +101,84 @@ app.post('/events', function (req, res) {
 // This route handles POST requests for Slack events
 app.post('/action', function (req, res) {
 
-    // Respond to Slack with 200 OK
-    res.sendStatus(200);
-
     console.log("\n============ NEW ACTION ============");
     console.log("===================================");
 
-    // Print the Event API Request body
-    console.log(req.body);
+    // Respond to Slack with 200 OK
+    res.sendStatus(200);
+
+    // Parse the payload
+    let action = JSON.parse(req.body.payload);
+
+    // Log the request payload
+    console.log(action);
 
     console.log("###################################");
 
+    if (action.actions[0].value == "get-started-home-button") {
+        modalView = {
+            "type": "modal",
+            "callback_id": "brewery-manager-app-home-modal",
+            "notify_on_close": false,
+            "title": {
+                "type": "plain_text",
+                "text": "Brews",
+                "emoji": true
+            },
+            "close": {
+                "type": "plain_text",
+                "text": "Close",
+                "emoji": true
+            },
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Brew 1"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Brew 2"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Brew 3"
+                    }
+                }
+            ]
+        }
+        openModal(action.trigger_id, modalView);
+    }
+
 });
+
+function openModal(trigger_id, view) {
+
+    console.log("\n============ OPEN MODAL ============");
+    console.log("===================================");
+
+    axios.post("https://slack.com/api/views.open", {
+        trigger_id: trigger_id,
+        view: view
+
+    }, {
+        headers: {
+            'Authorization': `Bearer ${botToken}`,
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    })
+        .then(response => {
+            console.log(response.data)
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error.response)
+        });
+}
