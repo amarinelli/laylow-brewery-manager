24bbe6a2e26824d6547465ad1c65957c2533189c
Adam Marinelli <a.marinelli16@gmail.com>
2019-12-19 04:31:39 -0500

    Integrated airtable data into modal

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('dotenv').config()
 const appId = process.env.APP_ID;
 const botToken = process.env.BOT_TOKEN;
 const airtableToken = process.env.AIRTABLE_TOKEN;
+const airtableBase = process.env.AIRTABLE_BASE;
 
 let appHomeView = {
     "type": "home",
@@ -82,7 +83,6 @@ app.post('/events', function (req, res) {
         res.sendStatus(200);
 
         console.log("\n============ NEW EVENT ============");
-        console.log("===================================");
 
         // Print the Event API Request body
         console.log(JSON.stringify(req.body, null, 4));
@@ -93,8 +93,6 @@ app.post('/events', function (req, res) {
             console.log("App Home Opened");
         }
 
-        console.log("###################################");
-
     }
 });
 
@@ -102,7 +100,6 @@ app.post('/events', function (req, res) {
 app.post('/action', function (req, res) {
 
     console.log("\n============ NEW ACTION ============");
-    console.log("===================================");
 
     // Respond to Slack with 200 OK
     res.sendStatus(200);
@@ -113,56 +110,132 @@ app.post('/action', function (req, res) {
     // Log the request payload
     console.log(action);
 
-    console.log("###################################");
-
     if (action.actions[0].value == "get-started-home-button") {
-        modalView = {
-            "type": "modal",
-            "callback_id": "brewery-manager-app-home-modal",
-            "notify_on_close": false,
-            "title": {
-                "type": "plain_text",
-                "text": "Brews",
-                "emoji": true
-            },
-            "close": {
-                "type": "plain_text",
-                "text": "Close",
-                "emoji": true
-            },
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Brew 1"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Brew 2"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Brew 3"
-                    }
-                }
-            ]
-        }
-        openModal(action.trigger_id, modalView);
-    }
 
+        async function listBrewsOpenModal() {
+            const brews = await listBrews(maxRecords = 6);
+            const modal = openModal(action.trigger_id, {
+                "type": "modal",
+                "callback_id": "brewery-manager-app-home-modal",
+                "notify_on_close": false,
+                "title": {
+                    "type": "plain_text",
+                    "text": "Past 6 brews",
+                    "emoji": true
+                },
+                "close": {
+                    "type": "plain_text",
+                    "text": "Close",
+                    "emoji": true
+                },
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": `*${brews.records[0].fields["Brew Code"]}*`
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": brews.records[0].fields["Brew Date"]
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": `*${brews.records[1].fields["Brew Code"]}*`
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": brews.records[1].fields["Brew Date"]
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": `*${brews.records[2].fields["Brew Code"]}*`
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": brews.records[2].fields["Brew Date"]
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": `*${brews.records[3].fields["Brew Code"]}*`
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": brews.records[3].fields["Brew Date"]
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": `*${brews.records[4].fields["Brew Code"]}*`
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": brews.records[4].fields["Brew Date"]
+                            }
+                        ]
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": `*${brews.records[5].fields["Brew Code"]}*`
+                        }
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": brews.records[5].fields["Brew Date"]
+                            }
+                        ]
+                    }
+                ]
+            });
+        };
+
+        listBrewsOpenModal();
+    }
 });
 
 function openModal(trigger_id, view) {
 
     console.log("\n============ OPEN MODAL ============");
-    console.log("===================================");
 
     axios.post("https://slack.com/api/views.open", {
         trigger_id: trigger_id,
@@ -170,7 +243,7 @@ function openModal(trigger_id, view) {
 
     }, {
         headers: {
-            'Authorization': `Bearer ${botToken}`,
+            "Authorization": `Bearer ${botToken}`,
             "Content-Type": "application/json; charset=utf-8"
         }
     })
@@ -180,5 +253,36 @@ function openModal(trigger_id, view) {
         })
         .catch(error => {
             console.log(error.response)
+            return error.response
         });
-}
+};
+
+function listBrews(maxRecords) {
+
+    console.log("\n============ LIST BREWS ============");
+
+    return new Promise(resolve => {
+
+        let config = {
+            headers: { 'Authorization': `Bearer ${airtableToken}` },
+
+            // TODO Only request necessary "fields"
+            params: {
+                "maxRecords": maxRecords,
+                "view": "Ongoing"
+            },
+        }
+
+        axios.get(`https://api.airtable.com/v0/${airtableBase}/Brew`, config)
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+                resolve(response.data)
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error.response.status, error.response.statusText);
+            })
+
+    });
+};
